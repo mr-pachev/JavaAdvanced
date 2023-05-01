7adb1c4cac1ebf89d25b59d342b8f9e77888e59d
Ivan Pachev <mr.pachev@abv.bg>
2023-05-01 20:35:37 +0300

    Update _01_BrowserHistory.java

--- src/Lab/_01_BrowserHistory.java
+++ src/Lab/_01_BrowserHistory.java
@@ -7,14 +7,25 @@ public class _01_BrowserHistory {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine();
         ArrayDeque<String> url = new ArrayDeque<>();
 
-        while (true){
+        String input = scanner.nextLine();
 
+        while (!input.equals("Home")) {
 
+            if (input.equals("back")) {
+                if (url.size() == 1) {
+                    System.out.println("no previous URLs");
+                    input = scanner.nextLine();
+                    continue;
+                } else {
+                    url.pop();
+                }
+            } else {
+                url.push(input);
+            }
 
-
+            System.out.println(url.peek());
             input = scanner.nextLine();
         }
 
