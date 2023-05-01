3ff1423ff59c8ff9237153281d07981e1c0efbb9
Ivan Pachev <mr.pachev@abv.bg>
2023-05-01 17:05:30 +0300

    BrowserHistory

---
+++ src/Lab/_01_BrowserHistory.java
@@ -0,0 +1,22 @@
+package Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _01_BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        ArrayDeque<String> url = new ArrayDeque<>();
+
+        while (true){
+
+
+
+
+            input = scanner.nextLine();
+        }
+
+    }
+}
