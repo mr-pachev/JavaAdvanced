f7efc12507cc7e84b939eaf4ea7b3c261f56afaa
Ivan Pachev <mr.pachev@abv.bg>
2023-05-04 09:45:30 +0300

    _01_BasicStackOperations

---
+++ src/Exercise/_01_BasicStackOperations.java
@@ -0,0 +1,11 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class _01_BasicStackOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
--- src/Exercise/_01_ReverseNumbersWithAStack.java
+++ src/Exercise/_01_ReverseNumbersWithAStack.java
@@ -1,10 +1,25 @@
 package Exercise;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class _01_ReverseNumbersWithAStack {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
-    
+        List<Integer> inputList = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int n : inputList) {
+            stack.push(n);
+        }
+
+        while (stack.size() > 0){
+            System.out.print(stack.pop() + " ");
+        }
+    }
 }
