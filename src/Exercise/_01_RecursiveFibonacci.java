7f5cd19cf207e2a2eae9e32967494b6c208c282f
Ivan Pachev <mr.pachev@abv.bg>
2023-05-04 18:43:30 +0300

    Update _01_RecursiveFibonacci.java

--- src/Exercise/_01_RecursiveFibonacci.java
+++ src/Exercise/_01_RecursiveFibonacci.java
@@ -8,23 +8,24 @@ public class _01_RecursiveFibonacci {
 
         Scanner input = new Scanner(System.in);
 
-        ArrayDeque<Long> Fibonacci = new ArrayDeque<>();
+        ArrayDeque<Long> queue = new ArrayDeque<>(); //опашка, която ще съдържа поредното число на Фибоначи
+
         long N = Integer.parseInt(input.nextLine());
-        System.out.println(getFibonacci(Fibonacci, N));
+        System.out.println(getFibonacci(queue, N));
     }
 
-    private static Long getFibonacci(ArrayDeque<Long> Fibonacci, Long N) {
-        if (N < 2) {
+    private static Long getFibonacci(ArrayDeque<Long> FibonacciQueue, Long N) {
+        if (N < 2) {    //подсигуряване, че числото, на което се търси числото на Фибоначи ще запобне най-малко от 2
             return 1L;
         } else {
-            Fibonacci.offer(0L);
-            Fibonacci.offer(1L);
+            FibonacciQueue.offer(0L);
+            FibonacciQueue.offer(1L);
             for (int i = 0; i < N; i++) {
-                long Sum = Fibonacci.poll() + Fibonacci.peek();
-                Fibonacci.offer(Sum);
+                long Sum = FibonacciQueue.poll() + FibonacciQueue.peek();
+                FibonacciQueue.offer(Sum);
             }
         }
-        Fibonacci.poll();
-        return Fibonacci.peek();
+        FibonacciQueue.poll();
+        return FibonacciQueue.peek();
     }
 }
