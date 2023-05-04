69a0e1602949835d350a39ae774aecff72e383c3
Ivan Pachev <mr.pachev@abv.bg>
2023-05-04 10:08:32 +0300

    Update _01_BasicStackOperations.java

--- src/Exercise/_01_BasicStackOperations.java
+++ src/Exercise/_01_BasicStackOperations.java
@@ -1,11 +1,38 @@
 package Exercise;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class _01_BasicStackOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        int n = scanner.nextInt();
+        int s = scanner.nextInt();
+        int x = scanner.nextInt();
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 1; i <= n; i++) {
+            stack.push(scanner.nextInt());
+        }
+
+        for (int i = 1; i <= s; i++) {
+            if (!stack.isEmpty()) {
+                stack.pop();
+            }
+        }
+
+        if (stack.contains(x)) {
+            System.out.println("true");
+        } else {
+            if (stack.isEmpty()) {
+                System.out.println("0");
+            } else {
+                System.out.println(Collections.min(stack));
+            }
+        }
+
     }
 }
