4761b0a834bb457aa2d631971e9bd8677f481546
Ivan Pachev <mr.pachev@abv.bg>
2023-05-11 14:34:47 +0300

    Update _02_MaximumSumOf2x2Submatrix.java

--- src/Lab/_02_MaximumSumOf2x2Submatrix.java
+++ src/Lab/_02_MaximumSumOf2x2Submatrix.java
@@ -12,29 +12,33 @@ public class _02_MaximumSumOf2x2Submatrix {
 
         int[][] matrix = new int[row][colum];
 
+        //пълнене на матрицата
         for (int i = 0; i < row; i++) {
             input = scanner.nextLine();
             for (int j = 0; j < colum; j++) {
                 matrix[i][j] = Integer.parseInt(input.split(", ")[j]);
             }
         }
-
+        //maxSum е първата 2x2 от матрицата
         int maxSum = matrix[0][0] + matrix[0][1] + matrix[1][0] + matrix[1][1];
-        int currentSum = 0;
-        int currentRow = 0;
-        int currentColum = 0;
+
+        int currentRow = 0;     //конретния ред започва от първия ред
+        int currentColum = 0;   //конретната колона започва от първата колона
+
+        //обхождането на матрицата е събразено да има 2x2
         for (int i = 0; i < row - 1; i++) {
             for (int j = 0; j < colum - 1; j++) {
-               currentSum = matrix[i][j] + matrix[i][j+1] + matrix[i+1][j] + matrix[i+1][j+1];
-               if (currentSum >= maxSum){
-                   maxSum = currentSum;
-                   currentRow = i;
-                   currentColum = j;
-               }
+                int currentSum = matrix[i][j] + matrix[i][j + 1] + matrix[i + 1][j] + matrix[i + 1][j + 1];
+
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                    currentRow = i;
+                    currentColum = j;
+                }
             }
         }
-        System.out.printf("%d %d%n", matrix[currentRow][currentColum], matrix[currentRow][currentColum+1]);
-        System.out.printf("%d %d%n", matrix[currentRow+1][currentColum], matrix[currentRow+1][currentColum+1]);
+        System.out.printf("%d %d%n", matrix[currentRow][currentColum], matrix[currentRow][currentColum + 1]);
+        System.out.printf("%d %d%n", matrix[currentRow + 1][currentColum], matrix[currentRow + 1][currentColum + 1]);
         System.out.println(maxSum);
     }
 }
