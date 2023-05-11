7448a87c0ca10a6226071455c843f4d7184ef235
Ivan Pachev <mr.pachev@abv.bg>
2023-05-11 11:50:16 +0300

    Update _02_SumMatrixElements.java

--- src/Lab/_02_SumMatrixElements.java
+++ src/Lab/_02_SumMatrixElements.java
@@ -17,12 +17,22 @@ public class _02_SumMatrixElements {
             input = scanner.nextLine();
             for (int j = 0; j < colum; j++) {
                 int current = Integer.parseInt(input.split(", ")[j]);
-                matrix[i][j] = current;
+                matrix(row, colum, current);
                 sum += current;
             }
         }
         System.out.println(row);
         System.out.println(colum);
         System.out.println(sum);
+    }
+    //метод за зъпълване на матрица от тип int
+    public static int[][] matrix(int row, int colum, int insert){
+        int[][] matrix = new int[row][colum];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < colum; j++) {
+                matrix[i][j] = insert;
+            }
+        }
+       return matrix;
     }
 }
