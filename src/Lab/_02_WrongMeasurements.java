7989785e6fb6c618f012d2f8cf90160765a75bca
Ivan Pachev <mr.pachev@abv.bg>
2023-05-12 19:25:36 +0300

    Update _02_WrongMeasurements.java

--- src/Lab/_02_WrongMeasurements.java
+++ src/Lab/_02_WrongMeasurements.java
@@ -20,7 +20,10 @@ public class _02_WrongMeasurements {
                     .toArray();
 
             matrix[i] = columElements;
-            changeMatrix[i] = columElements;
+        }
+
+        for (int i = 0; i < row; i++) {
+            changeMatrix[i] = matrix[i].clone();
         }
 
         int[] wrong = Arrays.stream(scanner.nextLine()
@@ -30,28 +33,111 @@ public class _02_WrongMeasurements {
 
         int wrongValue = matrix[wrong[0]][wrong[1]];
 
-        for (int i = 0; i < row; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 int sum = 0;
 
-                if (matrix[i][j] == wrongValue){                            //проверка дали дадения елемент съпвада с грешния
+                if (matrix[i][j] == wrongValue) {                             //проверка дали дадения елемент съпвада с грешния
 
-                    if(i == 0 && j == 0){                                   //първи ред и първа колона
-                        if (matrix[i+1][j] != wrongValue && matrix[i][j+1] != wrongValue){
-                            sum = matrix[i+1][j] + matrix[i][j+1];
+                    if (i == 0 && j == 0) {                                   //първи ред и първа колона
+                        if (matrix[i + 1][j] != wrongValue) {                 //долу
+                            sum += matrix[i + 1][j];
                         }
-                    }else if (i == 0 && j == matrix[i].length - 1){         //първи ред и последна колона
-                        
-                    }else if (i == row - 1 && j == 0){                      //последен ред и първа колона
+                        if (matrix[i][j + 1] != wrongValue) {                 //дясно
+                            sum += matrix[i][j + 1];
+                        }
 
-                    }else if (i == row - 1 && j == matrix[i].length - 1){   //последен ред и последна колона
+                    } else if (i == 0 && j == matrix[i].length - 1) {         //първи ред и последна колона
+                        if (matrix[i + 1][j] != wrongValue) {                 //долу
+                            sum += matrix[i + 1][j];
+                        }
+                        if (matrix[i][j - 1] != wrongValue) {                 //ляво
+                            sum += matrix[i][j - 1];
+                        }
 
+                    } else if (i == row - 1 && j == 0) {                      //последен ред и първа колона
+                        if (matrix[i - 1][j] != wrongValue) {                 //горе
+                            sum += matrix[i - 1][j];
+                        }
+                        if (matrix[i][j + 1] != wrongValue) {                 //дясно
+                            sum += matrix[i][j + 1];
+                        }
+
+                    } else if (i == row - 1 && j == matrix[i].length - 1) {   //последен ред и последна колона
+                        if (matrix[i - 1][j] != wrongValue) {                 //горе
+                            sum += matrix[i - 1][j];
+                        }
+                        if (matrix[i][j - 1] != wrongValue) {                 //ляво
+                            sum += matrix[i][j - 1];
+                        }
+
+                    } else if (i == 0) {                                       //първи ред
+                        if (matrix[i + 1][j] != wrongValue) {                  //долу
+                            sum += matrix[i + 1][j];
+                        }
+                        if (matrix[i][j - 1] != wrongValue) {                  //ляво
+                            sum += matrix[i][j - 1];
+                        }
+                        if (matrix[i][j + 1] != wrongValue) {                  //дясно
+                            sum += matrix[i][j + 1];
+                        }
+
+                    } else if (i == row - 1) {                                 //последен ред
+                        if (matrix[i - 1][j] != wrongValue) {                  //горе
+                            sum += matrix[i - 1][j];
+                        }
+                        if (matrix[i][j - 1] != wrongValue) {                  //ляво
+                            sum += matrix[i][j - 1];
+                        }
+                        if (matrix[i][j + 1] != wrongValue) {                  //дясно
+                            sum += matrix[i][j + 1];
+                        }
+
+                    } else if (j == 0) {                                        //първа колона
+                        if (matrix[i - 1][j] != wrongValue) {                  //горе
+                            sum += matrix[i - 1][j];
+                        }
+                        if (matrix[i + 1][j] != wrongValue) {                  //долу
+                            sum += matrix[i + 1][j];
+                        }
+                        if (matrix[i][j + 1] != wrongValue) {                  //дясно
+                            sum += matrix[i][j + 1];
+                        }
+
+                    } else if (j == matrix[i].length - 1) {                      //последна колона
+                        if (matrix[i - 1][j] != wrongValue) {                  //горе
+                            sum += matrix[i - 1][j];
+                        }
+                        if (matrix[i + 1][j] != wrongValue) {                  //долу
+                            sum += matrix[i + 1][j];
+                        }
+                        if (matrix[i][j - 1] != wrongValue) {                  //ляво
+                            sum += matrix[i][j - 1];
+                        }
+                    } else {
+                        if (matrix[i - 1][j] != wrongValue) {                  //горе
+                            sum += matrix[i - 1][j];
+                        }
+                        if (matrix[i + 1][j] != wrongValue) {                  //долу
+                            sum += matrix[i + 1][j];
+                        }
+                        if (matrix[i][j - 1] != wrongValue) {                  //ляво
+                            sum += matrix[i][j - 1];
+                        }
+                        if (matrix[i][j + 1] != wrongValue) {                  //дясно
+                            sum += matrix[i][j + 1];
+                        }
                     }
 
-
+                    changeMatrix[i][j] = sum;
                 }
             }
         }
-
+        for (int i = 0; i < changeMatrix.length; i++) {
+            for (int j = 0; j < changeMatrix[i].length; j++) {
+                System.out.printf("%d ", changeMatrix[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
