6460339c8d504bbc24436ac0cbd1fb35511daeb6
Ivan Pachev <mr.pachev@abv.bg>
2023-05-18 20:17:49 +0300

    Create _03_SetsOfElements.java

---
+++ src/Exercise/_03_SetsOfElements.java
@@ -0,0 +1,31 @@
+package Exercise;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class _03_SetsOfElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] setSize = Arrays.stream(scanner.nextLine()
+                                .split(" "))
+                                .mapToInt(Integer::parseInt)
+                                 .toArray();
+
+        Set<Integer> setOne = new LinkedHashSet<>();
+        Set<Integer> setTwo = new LinkedHashSet<>();
+
+        for (int i = 1; i <= setSize[0]; i++) {
+            setOne.add(Integer.parseInt(scanner.nextLine()));
+        }
+
+        for (int i = 1; i <= setSize[1]; i++) {
+            setTwo.add(Integer.parseInt(scanner.nextLine()));
+        }
+        setOne.retainAll(setTwo);
+
+        setOne.forEach(element -> System.out.printf("%d ", element));
+    }
+}
