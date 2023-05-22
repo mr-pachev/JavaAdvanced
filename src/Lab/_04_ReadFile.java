0aa40140a69d85302121a0e1b7466ed738293e87
Ivan Pachev <mr.pachev@abv.bg>
2023-05-22 10:37:01 +0300

    _04_WriteToFile

--- src/Lab/_04_ReadFile.java
+++ src/Lab/_04_ReadFile.java
@@ -8,7 +8,7 @@ public class _04_ReadFile {
     public static void main(String[] args) throws IOException {
         String path = "E:\\GitHubRepo\\JavaAdvanced\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
 
-        FileInputStream inputStream = new FileInputStream(path);
+        FileInputStream inputStream = new FileInputStream(path);            //отваряне на поток за четене
 
         int currentByte = inputStream.read();                               //променлива, която ще държи конкретния прочетен байт от зададения път на файла с помоща на read
 
@@ -18,5 +18,6 @@ public class _04_ReadFile {
             currentByte = inputStream.read();
         }
 
+        inputStream.close();                                                 //затваряне на потока
     }
 }
---
+++ src/Lab/_04_WriteToFile.java
@@ -0,0 +1,31 @@
+package Lab;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class _04_WriteToFile {
+    public static void main(String[] args) throws IOException {
+
+        String inputPath = "E:\\GitHubRepo\\JavaAdvanced\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams\\input.txt";
+        String outputPath = "E:\\GitHubRepo\\JavaAdvanced\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams\\output.txt";
+
+        FileInputStream inputStream = new FileInputStream(inputPath);       //отваряне на поток за четене
+        FileOutputStream outputStream = new FileOutputStream(outputPath);   //отваряне на поток за писане
+
+        int currentByte = inputStream.read();
+
+        while ((int) currentByte > 0) {                                    //подсигуряваме си четене на всички символи от файла
+            char currentSymbol = (char) currentByte;                       //представяне на int, като символ
+
+            if (currentByte != '.' && currentByte != ',' && currentByte != '!' && currentByte != '?') {
+                outputStream.write(currentByte);
+            }
+
+            currentByte = inputStream.read();
+        }
+
+        inputStream.close();
+        outputStream.close();
+    }
+}
