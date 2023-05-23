833465807b0453c4f6e64398814d767986c402c9
Ivan Pachev <mr.pachev@abv.bg>
2023-05-23 18:38:17 +0300

    Update ThroneConquering.java

--- src/Exams/ThroneConquering.java
+++ src/Exams/ThroneConquering.java
@@ -115,7 +115,7 @@ public class ThroneConquering {
     private static void printMatrix(char[][] matrix) {
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix[0].length; col++) {
-                System.out.print(matrix[row][col] + " ");
+                System.out.print(matrix[row][col]);
             }
             System.out.println(); //свали курсора на следващия ред
         }
