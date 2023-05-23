c3edfd310b01f2a1a1333c232a0f014d60e813d3
Ivan Pachev <mr.pachev@abv.bg>
2023-05-23 10:43:55 +0300

    _04_ALLCAPITALS

---
+++ src/Exercise/_04_Exerc/_04_ALLCAPITALS.java
@@ -0,0 +1,7 @@
+package Exercise._04_Exerc;
+
+public class _04_ALLCAPITALS {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Exercise/_04_Exerc/_04_SumBytes.java
+++ src/Exercise/_04_Exerc/_04_SumBytes.java
@@ -1,31 +1,28 @@
 package Exercise._04_Exerc;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
-public class _04_SumLines {
+public class _04_SumBytes {
     public static void main(String[] args) {
+
         String inputPath = "E:\\GitHubRepo\\JavaAdvanced\\src\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
 
         try (BufferedReader reader = Files.newBufferedReader(Path.of(inputPath));) {
             String line = reader.readLine();
 
+            int count = 0;
             while (line != null) {                          //върти цикъла, докато има редове за чете от файла
-                int count = 0;
 
                 for (int i = 0; i < line.length(); i++) {
                     int asciiCode = line.codePointAt(i);    //взима ascii кода на всеки конкретен символ
                     count += asciiCode;
                 }
-
-                System.out.println(count);
                 line = reader.readLine();
             }
-
+            System.out.println(count);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
