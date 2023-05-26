3dd44a2f6ce73ae569fa26dac511d4182eeb84c2
Ivan Pachev <mr.pachev@abv.bg>
2023-05-26 21:08:19 +0300

    _05_ThePartyReservationFilterModule

--- src/Exercise/_05_Exerc/_05_ListOfPredicates.java
+++ src/Exercise/_05_Exerc/_05_ListOfPredicates.java
@@ -1,6 +1,7 @@
 package Exercise._05_Exerc;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.BiPredicate;
@@ -11,27 +12,60 @@ public class _05_ListOfPredicates {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        List<String> names = Arrays.stream(scanner.nextLine().split(" ")).collect(Collectors.toList());
 
-        List<Integer> numbersList = Arrays.stream(scanner.nextLine().split(" "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        String input;
+        while (!"Party!".equals(input = scanner.nextLine()) && !names.isEmpty()) {
+            String[] tokens = input.split(" ");
+            String command = tokens[0];
+            String secondCommand = tokens[1];
 
-        BiPredicate<List<Integer>, Integer> isDivision = (list, a) -> {
-            for (Integer s : list){
-                if (a % s != 0){
-                    return false;
+            if (command.equals("Remove")) {
+                if (secondCommand.equals("StartsWith")) {
+                    String lastCommand = tokens[2];
+                    names = names.stream().filter(name -> !name.startsWith(lastCommand)).collect(Collectors.toList());
+                } else if (secondCommand.equals("EndsWith")) {
+                    String lastCommand = tokens[2];
+                    names = names.stream().filter(name -> !name.endsWith(lastCommand)).collect(Collectors.toList());
+                } else if (secondCommand.equals("Length")) {
+                    int namesLength = Integer.parseInt(tokens[2]);
+                    names = names.stream().filter(name -> name.length() != namesLength).collect(Collectors.toList());
                 }
-            }
-            return  true;
-        };
 
-        for (int i = 1; i <= n; i++) {
-            if (isDivision.test(numbersList, i)){
-                System.out.print(i + " ");
+            } else if (command.equals("Double")) {
+                if (secondCommand.equals("StartsWith")) {
+                    String lastCommand = tokens[2];
+                    BiPredicate<String, String> doubleStartsWithPredicate = String::startsWith;
+                    List<String> current = names.stream().filter(name -> doubleStartsWithPredicate.test(name, lastCommand)).collect(Collectors.toList());
+                    for (int i = 0; i < current.size(); i++) {
+                        names.add(names.indexOf(current.get(i)), current.get(i));
+                    }
+
+                } else if (secondCommand.equals("EndsWith")) {
+                    String lastCommand = tokens[2];
+                    BiPredicate<String, String> doubleEndsWithPredicate = String::endsWith;
+                    List<String> current = names.stream().filter(name -> doubleEndsWithPredicate.test(name, lastCommand)).collect(Collectors.toList());
+                    for (int i = 0; i < current.size(); i++) {
+                        names.add(names.indexOf(current.get(i)), current.get(i));
+                    }
+                } else if (secondCommand.equals("Length")) {
+                    int namesLength = Integer.parseInt(tokens[2]);
+                    BiPredicate<String, Integer> doubleLengthPredicate = (name, length) -> name.length() == length;
+                    List<String> current = names.stream().filter(name -> doubleLengthPredicate.test(name, namesLength)).collect(Collectors.toList());
+                    for (int i = 0; i < current.size(); i++) {
+                        names.add(names.indexOf(current.get(i)), current.get(i));
+                    }
+
+                }
             }
         }
 
+        Collections.sort(names);
+        if (!names.isEmpty()) {
 
+            System.out.println(String.join(", ", names) + " are going to the party!");
+        } else {
+            System.out.println("Nobody is going to the party!");
+        }
     }
 }
