ce128b312fec74ee4c397c994142a58d1e8e3fde
Ivan Pachev <mr.pachev@abv.bg>
2023-05-26 14:47:27 +0300

    Update _05_KnightsOfHonor.java

--- src/Exercise/_05_Exerc/_05_KnightsOfHonor.java
+++ src/Exercise/_05_Exerc/_05_KnightsOfHonor.java
@@ -1,4 +1,18 @@
 package Exercise._05_Exerc;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
 public class _05_KnightsOfHonor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] arr = scanner.nextLine().split("\\s+");
+
+        Consumer<String> printElement = el -> System.out.printf("Sir %s%n", el);
+
+        Arrays.stream(arr).forEach(printElement);
+
+    }
 }
