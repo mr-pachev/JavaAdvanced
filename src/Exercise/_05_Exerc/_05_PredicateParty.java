7da3314031117ca5029a9a39bb283915b03e029e
Ivan Pachev <mr.pachev@abv.bg>
2023-05-27 11:36:10 +0300

    Update _05_PredicateParty.java

--- src/Exercise/_05_Exerc/_05_PredicateParty.java
+++ src/Exercise/_05_Exerc/_05_PredicateParty.java
@@ -8,51 +8,56 @@ public class _05_PredicateParty {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<String> guests = Arrays.stream(scanner.nextLine().split("\\s+"))
+        ArrayList<String> guestsList = Arrays.stream(scanner.nextLine().split("\\s+"))
                                           .collect(Collectors.toCollection(ArrayList::new));
 
-        String line = scanner.nextLine();
+        String inputData = scanner.nextLine();
 
-        while (!line.equals("Party!")) {
+        while (!inputData.equals("Party!")) {
 
-            Predicate<String> predicate = null;
+            Predicate<String> predicate = null;                             //създаваме УСЛОВИЕ, което ще е различно според даденият случай
 
-            String criteria = line.split("\\s+")[2];
+            String criteria = inputData.split("\\s+")[2];             //взима последната част от входните данни
 
-            if (line.contains("Length")) {
-                predicate = x -> x.length() == Integer.parseInt(criteria);
-            } else if (line.contains("StartsWith")) {
-                predicate = x -> x.startsWith(criteria);
+            if (inputData.contains("Length")) {                             //когато съдържа Length
+                predicate = x -> x.length() == Integer.parseInt(criteria);  //задава се УСЛОВИЕТО -> проверява дали зададения String е с дължината на criteria
+
+            } else if (inputData.contains("StartsWith")) {                  //когато съдържа StartWith
+                predicate = x -> x.startsWith(criteria);                    //задава се УСЛОВИЕТО -> проверява дали началото на зададения String съдържа criteria
+
             } else {
-                predicate = x -> x.endsWith(criteria);
+                predicate = x -> x.endsWith(criteria);                      //задава се УСЛОВИЕТО -> проверява дали края на зададения String съдържа criteria
             }
 
-            if (line.contains("Remove")) {
-                ArrayList<String> toRemove = new ArrayList<>();
-                for (String guest : guests) {
-                    if (predicate.test(guest)) {
-                        toRemove.add(guest);
-                    }
-                }
+            //след зададеното УСЛОВИЕ поверка какво трябва да се направи спрямо него
+            if (inputData.contains("Remove")) {                             //премахване на елементи спрямо УСЛОВИЕТО
+                ArrayList<String> toRemoveList = new ArrayList<>();         //създава се лист с елементи за премахване
 
-                guests.removeAll(toRemove);
-            } else {
-                ArrayList<String> toAdd = new ArrayList<>();
-                for (String guest : guests) {
+                for (String guest : guestsList) {                           //обхожда questLis и при отговаряне на зададеното УСЛОВИЕ добавя елемента към toRemoveList
                     if (predicate.test(guest)) {
-                        toAdd.add(guest);
+                        toRemoveList.add(guest);
                     }
                 }
-                guests.addAll(toAdd);
+                guestsList.removeAll(toRemoveList);
+
+            } else {                                                          //добавяне на елементи спрямо УСЛОВИЕТО
+                ArrayList<String> toAddList = new ArrayList<>();              //създава се лист с елементи за добавяне
+
+                for (String guest : guestsList) {                             //обхожда questLis и при отговаряне на зададеното УСЛОВИЕ добавя елемента към toAddList
+                    if (predicate.test(guest)) {
+                        toAddList.add(guest);
+                    }
+                }
+                guestsList.addAll(toAddList);
             }
 
-            line = scanner.nextLine();
+            inputData = scanner.nextLine();
         }
 
-        if (guests.isEmpty()) {
+        if (guestsList.isEmpty()) {
             System.out.println("Nobody is going to the party!");
         } else {
-            System.out.println(guests.stream().sorted().collect(Collectors.toCollection(ArrayList::new)).toString().replaceAll("([\\[\\]])", "") + " are going to the party!");
+            System.out.println(guestsList.stream().sorted().collect(Collectors.toCollection(ArrayList::new)).toString().replaceAll("([\\[\\]])", "") + " are going to the party!");
         }
     }
 }
