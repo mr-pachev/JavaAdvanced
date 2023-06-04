a6d208ffb040b01cbe58b452ef3a0aa43d5eeb6b
Ivan Pachev <mr.pachev@abv.bg>
2023-06-04 16:20:19 +0300

    Update

--- src/_08_Lab_ListUtilities/Main.java
+++ src/_08_Lab_ListUtilities/Main.java
@@ -3,7 +3,7 @@ package _08_Lab_ListUtilities;
 import java.util.Arrays;
 import java.util.List;
 
-public class Mail {
+public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(5, 98, 55);
 
