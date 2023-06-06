c903ff99d14b3f737779b79b47202d9b110f4243
Ivan Pachev <mr.pachev@abv.bg>
2023-06-06 14:17:55 +0300

    _08_Exercise_GenericBox

---
+++ src/_08_Exercise_GenericBox/Box.java
@@ -0,0 +1,14 @@
+package _08_Exercise_GenericBox;
+
+public class Box <T>{
+    private T input;
+
+    public Box(T input) {
+        this.input = input;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s",this.input.getClass().getName(), this.input);
+    }
+}
