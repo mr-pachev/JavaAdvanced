e5158a87732d0b62d26a3b05dd5239e96543a302
Ivan Pachev <mr.pachev@abv.bg>
2023-06-07 21:39:22 +0300

    _09_Lab_BookComparator

---
+++ src/_09_Lab_BookComparator/Book.java
@@ -0,0 +1,50 @@
+package _09_Lab_BookComparator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book implements Comparable<Book> {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.title = title;
+        this.year = year;
+        this.authors = new ArrayList<>(Arrays.asList(authors));
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    @Override
+    public int compareTo(Book book) {
+        int result = this.title.compareTo(book.getTitle());
+        if (result == 0) {
+            return Integer.compare(this.year, book.getYear());
+        }
+        return result;
+    }
+}
